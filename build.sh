9c2fa639f29d04977615156a2682ee8dfb6808be
Juan Carrano <juan@carrano.com.ar>
2019-06-13 00:28:04 +0200

    Remove build.sh.
    Also, add .gitignore.

--- build.sh
+++
@@ -1,4 +0,0 @@
-gcc -pedantic -Wall -I./extra_libs -lSDL -ljack -ffast-math \
-	-fomit-frame-pointer -O2 -march=native rtfi.c images.c \
-	extra_libs/*.c extra_libs/libjc/*.c -o rtfi
-gcc -O2 -march=native -ljack mkmono.c -o mkmono
