f6dd54b08f577e5150408706b14b7c637c36ac74
Juan Carrano <juan@carrano.com.ar>
2019-06-12 22:34:43 +0200

    Random fixed I forgot to commit years ago.

--- iso226.py
+++ iso226.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #  untitled.py
-#  
+#
 #  Copyright 2012 Juan I Carrano <juan@superfreak.com.ar>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -28,7 +28,7 @@ f = np.array([20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400,
 	8000, 10000, 12500])
 
 af = np.array([0.532, 0.506, 0.480, 0.455, 0.432, 0.409, 0.387, 0.367, 0.349,
-	0.330, 0.315, 0.301, 0.288, 0.276, 0.267, 0.259, 0.253, 0.250, 0.246, 
+	0.330, 0.315, 0.301, 0.288, 0.276, 0.267, 0.259, 0.253, 0.250, 0.246,
 	0.244, 0.243, 0.243, 0.243, 0.242, 0.242, 0.245, 0.254, 0.271, 0.301])
 
 Lu = np.array([-31.6, -27.2, -23.0, -19.1, -15.9, -13.0, -10.3, -8.1, -6.2,
@@ -41,41 +41,41 @@ Tf = np.array([78.5, 68.7, 59.5, 51.1, 44.0, 37.5, 31.5, 26.5, 22.1, 17.9, 14.4,
 
 def iso226(phon, freqs = None):
 	"""Generates an Equal Loudness Contour as described in ISO 226
-	
+
 	This code is a translation of a Matlab function by Jeff Tacket.
-	Additionally, this version does a cubic spline interpolation if it is 
+	Additionally, this version does a cubic spline interpolation if it is
 	called with a vector of frequencies.
 	The original documentation is reproduced below:
-	
+
 	function [spl, freq] = iso226(phon):
-	
+
 	Usage:  [SPL FREQ] = ISO226(PHON);
-	
+
 		PHON is the phon value in dB SPL that you want the equal
 		loudness curve to represent. (1phon = 1dB @ 1kHz)
 		SPL is the Sound Pressure Level amplitude returned for
 		each of the 29 frequencies evaluated by ISO226.
 		FREQ is the returned vector of frequencies that ISO226
 		evaluates to generate the contour.
-	
+
 	Desc:   This function will return the equal loudness contour for
 		your desired phon level.  The frequencies evaulated in this
 		function only span from 20Hz - 12.5kHz, and only 29 selective
 		frequencies are covered.  This is the limitation of the ISO
 		standard.
-	
+
 		In addition the valid phon range should be 0 - 90 dB SPL.
 		Values outside this range do not have experimental values
 		and their contours should be treated as inaccurate.
-	
+
 		If more samples are required you should be able to easily
 		interpolate these values using spline().
-	
+
 	Author: Jeff Tackett 03/01/05 """
 
 
-	# TABLES FROM ISO226 
-	# (they were moved outside of the function)  
+	# TABLES FROM ISO226
+	# (they were moved outside of the function)
 
 	#Error Trapping
 	if phon < 0 or  phon > 90:
@@ -83,13 +83,30 @@ def iso226(phon, freqs = None):
 	else:
 		# Setup user-defined values for equation
 		Ln = phon;
-		
+
 		# Deriving sound pressure level from loudness level (iso226 sect 4.1)
 		Af=4.47E-3 * (10**(0.025*Ln) - 1.15) + (0.4*10**(((Tf+Lu)/10)-9 ))**af;
 		Lp=((10/af)*np.log10(Af)) - Lu + 94;
-		
+
 		#Return user data
 		if freqs is None:
-			return f, Lp;
+			return f, Lp
 		else:
 			return interp1d(f, Lp, 'cubic')(freqs)
+
+def test_plot():
+	import matplotlib.pyplot as plt
+
+	N_SAMPLES = 50
+
+	plt.figure()
+
+	f = np.geomspace(20, 12500, 150)
+
+	for i in range(9):
+		plt.plot(f, iso226(i*10, f))
+
+	plt.show()
+
+if __name__ == '__main__':
+	test_plot()
--- rtfi.py
+++ rtfi.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #  rtfi.py
-#  
+#
 #  Copyright 2012 Juan I Carrano <juan@superfreak.com.ar>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -27,51 +27,59 @@ from iso226 import iso226
 
 # Resonator Time-Frequency Image
 # This code calculates the parameters for a RTFI, as described in:
-# [1] R. Zhou and M. Mattavelli, “A new time-frequency representation for music 
-#	signal analysis: resonator time-frequency image,” in Proceedings of 
-#	the 9th International Symposium on Signal Processing and Its 
+# [1] R. Zhou and M. Mattavelli, “A new time-frequency representation for music
+#	signal analysis: resonator time-frequency image,” in Proceedings of
+#	the 9th International Symposium on Signal Processing and Its
 #	Applications (ISSPA ’07), Sharijah, UAE, February 2007.
-# [2] R. Zhou, “Feature extraction of musical content for automatic music 
-#	transcription” Ph.D. dissertation, Swiss Federal Inst. of Technol., 
+# [2] R. Zhou, “Feature extraction of musical content for automatic music
+#	transcription” Ph.D. dissertation, Swiss Federal Inst. of Technol.,
 #	Lausanne, Oct. 2006 [Online].
 #	Available: http://www.library.epfl.ch/en/theses/?nr=3638
-# [3] R. Zhou and J. D. Reiss, “A real-time frame-based multiple pitch 
-#	estimation method using the resonator time-frequency image,” in 
-#	Proceedings of the 3rd Music Information Retrieval Evaluation eXchange 
+# [3] R. Zhou and J. D. Reiss, “A real-time frame-based multiple pitch
+#	estimation method using the resonator time-frequency image,” in
+#	Proceedings of the 3rd Music Information Retrieval Evaluation eXchange
 #	(MIREX ’07), Vienna, Austria, September 2007.
-# [4] Ruohua Zhou, Joshua D. Reiss, Marco Mattavelli and Giorgio Zoia, "A 
+# [4] Ruohua Zhou, Joshua D. Reiss, Marco Mattavelli and Giorgio Zoia, "A
 #	Computationally Efficient Method for Polyphonic Pitch Estimation",
 #	EURASIP Journal on Advances in Signal Processing 2009, 2009 [Online].
 #	Available: http://asp.eurasipjournals.com/content/2009/1/729494
 
 def mtof(m):
-	"""midi number to frequency"""
+	"""MIDI number to frequency."""
 	return 440*2**((m - 69.0) / 12.0)
 
-def const_q(n0, n1, fxst):	
-	"""CONST_Q Generar una lista de f0 y Q para usar en rtfi con Q constante
-		n0: número midi de la freq de inicio (con 26 llegas a 36.7Hz)
-		n1: número midi de la freq final (con 116 llegas a ~6.6kHz)
-		fxst: cantidad de filtros por semitono (usá 10)
-		f0l : lista de frecuencias centrales (espaciadas logarítmicamente)
-		frw : lista de frw
-	OJO !!!!!
-	f0l está en frecuencia, mientras que frw está en frecuencia
-	angular. """
-	
+def const_q(n0, n1, fxst):
+	"""
+	Generate a list of center frequencies and bandwidth for a constant-Q filter
+	bank.
+
+	Parameters
+	----------
+
+	n0: lower frequency, given as MIDI number (i.e. 26 means 36.7Hz)
+	n1: upper frequency, given as MIDI number (i.e. 116 means 6.6kHz)
+	fxst: number of filters per semitone (10 is a good choice)
+
+	Returns
+	-------
+	f0l: list of center frequencies in Hz (they will be space logarithmically)
+	frw: list of bandwiths, in angular frequency (rad/s, or 2*pi*f)
+	pitches: midi numbers corresponding to the frequencies in f0l.
+	"""
+
 	n0_m = n0 - 69
 	n1_m = n1 - 69
 	step = 1.0/fxst
 	pitches = np.arange(n0, n1, step)
-	
+
 #	p0 = (pitches - 69.0) / 12.0
 #	f0l = 440*2**(p0)
 	f0l = mtof(pitches)
-	
+
 	d1 = 2**(1.0/(fxst*12))
 	c = (2.0 * d1 - 2) / (d1 + 1)
 	frw = f0l * 2 * np.pi * c
-	
+
 	return f0l, frw, pitches
 
 def f2band(f0l, n_harm):
@@ -88,31 +96,81 @@ def f2band(f0l, n_harm):
 	return bandlist
 
 def filter_coeffs(f0, fr_w, fs):
-	"""Coeficientes del filtro resonador complejo
-	f0: frecuencias centrales, fr_w anchos de banda (en rad/s), fs frec de muestreo"""
+	"""Generate the coefficients of the complex resonator filter.
+
+	The difference-equation for this filter is:
+		y[n] = k * x[n] + a1 * y[n-1]
+
+	Here k is a real gain and a1 is the complex parameter determining both the
+	frequency and the decay time of the filter.
+
+	Parameters
+	----------
+
+	f0: array of center frequencies (in Hz)
+	fr_w: array of equivalent rectangular bandwidths in rad/s.
+	fs: sample frequency in Hz.
+
+	Returns
+	-------
+
+	k: array of gains (real)
+	a1: resonant parameter (complex)
+	"""
 	fs = float(fs)
 	w0 = f0 * 2 * np.pi
 	ERB = fr_w
 	r_wm = ERB/np.pi
 	k = 1 - np.exp(-r_wm / fs)
 	a1 = np.exp((-r_wm + 1j * w0) / fs)
-	
+
 	return a1, k
 
+def resonator_run(a1, k, x):
+	#y =
+	pass
+
 def decimator_design(f0, fr_w, fs, att, force_n = None):
+	"""Design a low-pass decimating filter (using kaiser window) suitable for
+	feeding into a filter bank.
+
+	Given the frequencies and bandwidths of a filter bank, a filter is designed
+	such that all components of the back fit within the pass band. It is assumed
+	that during decimation the transition band with intersect with itself.
+
+	Parameters
+	----------
+
+	f0: list of filterbank center frequencies (in Hz). Only the last element
+		(highest frequency) will be considered.
+	frw: list of bandwidths, in angular frequency. Only the last element will be
+		considered.
+	fs: sample frequency (in Hz).
+	att: stopband attenuation in dB.
+	force_n: Fix the order if the filter. If not set, the order will be determined
+		automatically. If this is less than the required order, an error will
+		be thrown.
+
+	Returns
+	-------
+
+	fpass: end of the pass-band in Hz.
+	fstop: beggining of the stop-band in Hz.
+	h: impulse response.
+	"""
 	fpass = f0[-1] + frw[-1]/(2*np.pi)
 	fstop = fs/2 - fpass
-	
+
 	n, beta = sig.kaiserord(att, 2*(fstop-fpass)/float(fs))
-	
+
 	if force_n is not None:
 		if force_n < n:
 			raise Exception('Required order is more than specified')
 		else:
 			n = force_n
-	
+
 	h = sig.firwin(n, (fstop+fpass)/2, fstop-fpass,'kaiser', nyq = fs/2)
-	
+
 	return fpass, fstop, h
 
 
@@ -132,19 +190,19 @@ N_HARM = 10
 
 file_header = """/*
  * %s
- * 
+ *
  * Copyright 2012 Juan I Carrano <juan@superfreak.com.ar>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -152,7 +210,7 @@ file_header = """/*
  */
 
 /* ATTENTION:
-	This file was automatically generated. DO NOT edit by hand. Refer to 
+	This file was automatically generated. DO NOT edit by hand. Refer to
 	the rtfi.py program if you need to change the parameters.
 */
 
@@ -172,11 +230,11 @@ struct rtfi_param {
 param_template = """
 static const struct rtfi_param rtfi_{fs!s} =
 {{
-	.decfilter = {{ {h} 
+	.decfilter = {{ {h}
 	}},
-	.a1 = {{ {a1} 
+	.a1 = {{ {a1}
 	}},
-	.k = {{ {k} 
+	.k = {{ {k}
 	}},
 }};
 """
@@ -191,7 +249,7 @@ defines_footer = """
 
 spec_header = """/* ISO226 Equal loudness contour.
 Calculated for %f phon. Interpolated using cubic splines.
-Automatically generated file. DO NOT edit 
+Automatically generated file. DO NOT edit
 iso226[0] -> highest frequency */
 """
 
@@ -211,41 +269,44 @@ static const int hindex[N_HARM - 1] = {{
 """
 
 def list2carray(l):
-	return "\n\t".join(wrap(", ".join("%.16g"%x if not x.imag else 
+	"""Convert a numpy array into a C array for inclusion in a source file."""
+	return "\n\t".join(wrap(", ".join("%.16g"%x if not x.imag else
 			"%.16g + %.16g*I"%(x.real, x.imag) for x in l)))
 
 def create_params(f0, frw, fs, att, n):
 	upper_f0 = f0[-BLOCK:]
 	upper_frw = frw[-BLOCK:]
-	
+
 	a1, k = filter_coeffs(upper_f0, upper_frw, fs/2)
 	fpass, fstop, h = decimator_design(f0, frw, fs, att, n)
-	
+
 	if len(h) % 2:
 		raise RuntimeError("h must have an even number of coefficients")
-	
-	h = h[:len(h)/2] # remove redundancies
-	
-	return param_template.format(fs = fs, h = list2carray(h), 
+
+	h = h[:len(h)//2] # remove redundancies
+
+	return param_template.format(fs = fs, h = list2carray(h),
 				a1 = list2carray(a1), k = list2carray(k))
 
 def normiso(f0):
+	"""Get a iso226 curve normalized so that the minimum value is zero."""
 	x = iso226(ISOPHON, f0)
 	return x - min(x)
 
 def create_isocurve(f0):
+	"""Create a C array with the equal loudness contour for the frequencies in
+	f0. The minimum value will be zero."""
 	return isoarray.format(curve = list2carray(reversed(normiso(f0))))
-
 
 def allequal(l):
 	e = next(l)
 	return all(e == x for x in l)
-	
+
 def hindexes(f0, nh):
 	a1 = f2band(f0, nh)
 	# check offsets are constant
 	a1n = [[i - band[0] for i in band] for band in a1]
-	checks = (allequal(band[k] for band in a1n if len(band) > k) for k in 
+	checks = (allequal(band[k] for band in a1n if len(band) > k) for k in
 							range(len(a1[0])))
 	if all(checks):
 		return a1n[0]
@@ -254,124 +315,128 @@ def hindexes(f0, nh):
 
 def create_hindexes(f0, nh):
 	hi = hindexes(f0, nh)
-	
+
 	return hindex_template.format(hi = list2carray(hi[1:]))
 
 def define(f, k, v):
+	"""Write a preprocessor #define macro to the file object f."""
 	f.write("#define {0} {1}\n".format(k, v))
 
 if __name__ == '__main__':
 	import sys
-	
+
 	if 'write' in sys.argv:
 		fo = open(FILENAME, 'w+')
 		fd = open(AUXFILENAME, 'w+')
 		specf = open(SPECFILE, 'w+')
 	else:
 		fo = fd = specf = sys.stdout
-	
+
 	fo.write(file_header % (FILENAME, len(FS), list2carray(FS)))
-	
+
 	if 'write' in sys.argv:
 		fo.write('#include "%s"\n' % AUXFILENAME)
 		fd.write(defines_header.format(AUXFILENAME_CLEAN))
-	
+
 	define(fd, 'FXST', FXST)
 	define(fd, 'OCTAVE', OCTAVE)
 	define(fd, 'BLOCK', "(FXST*OCTAVE)")
-	
+
 	f0, frw, p = const_q(PINIT, PEND, FXST)
-	
+
 	nbands = len(f0)
 	steps = int(np.ceil(nbands / float(BLOCK)))
 	define(fd, 'RTFI_STEPS', steps)
 	define(fd, 'N_BANDS', nbands)
 	define(fd, 'REAL_N_BANDS', "((%d - %d) * FXST)" % (PEND, PINIT))
 	define(fd, 'LOWF_IGNORE', "(N_BANDS - REAL_N_BANDS)")
-	
+
 	maxbands = steps * BLOCK
 	bottom_minindex = excess = maxbands - nbands
 	define(fd, 'BOTTOM_MINIDEX', bottom_minindex)
-	
+
 	minfs = min(FS)
 	ma1, mk = filter_coeffs(f0, frw, minfs)
 	h = decimator_design(f0, frw, minfs, ATT)[-1]
-	#We designing the filter for the smallest Fs, for the other cases, 
+	#We designing the filter for the smallest Fs, for the other cases,
 	#the filter order will be smaller
 	maxn = len(h)
-	
+
 	define(fd, 'DFILTER_N', maxn)
-	
+
 	if 'write' in sys.argv:
 		fd.write(defines_footer % AUXFILENAME_CLEAN)
-	
+
 	fo.write(struct_def)
-	
+
 	for fs in FS:
 		fo.write(create_params(f0, frw, fs, ATT, maxn))
-	
+
 	fo.write("/* Automatically generated file ends here */\n")
-	
+
 	specf.write(spec_header % ISOPHON)
 	if 'write' in sys.argv:
 		specf.write('#include "%s"\n' % AUXFILENAME)
-	
+
 	define(specf, 'N_HARM', N_HARM);
 	specf.write(create_hindexes(f0, N_HARM))
 	specf.write(create_isocurve(f0))
 	specf.write(spec_footer)
-	
+
 	if 'plot' in  sys.argv:
 		import matplotlib.pyplot as plt
-		
+
 		plt.figure()
 		plt.stem(range(len(h)), h)
-		
+
 		plt.figure()
-		plt.hold(1)
-		
+
 		# let's illustrate the recursive decimation & filtering process
 		for i in range(steps):
 			current_fs = (0.0 + minfs)/(2**i)
 			w, H = sig.freqz(h, 1, 2048, whole = 0)
 			plt.plot(current_fs/2 * w/np.pi, 20*np.log10(abs(H)))
-			
+
 			band_top = -(BLOCK*i + 1)
 			band_bottom = band_top - BLOCK + 1
-			
+
 			a1_top = ma1[band_top]
 			k_top = mk[band_top]
-			
+
 			try:
 				a1_bott = ma1[band_bottom]
 			except IndexError:
 				band_bottom = 0;
 				a1_bott = ma1[band_bottom]
-			
+
 			k_bott = mk[band_bottom]
-				
+
 			w2, H2 = sig.freqz(k_top, np.array([1, (-a1_top)]), 50000, whole = 0)
 			w3, H3 = sig.freqz(k_bott, np.array([1, (-a1_bott)]), 50000, whole = 0)
 			p1 = plt.plot(minfs/2 * w2/np.pi, 20*np.log10(abs(H2)))[0]
 			plt.plot(minfs/2 * w3/np.pi, 20*np.log10(abs(H3)), color = p1.get_color())
-		
+
 		plt.plot(f0, normiso(f0))
-		
+
 		plt.figure()
-		
+
 		f, H = sig.freqz(h, 1, 2048, whole = 0)
 		f *= (FS[0]/2)/np.pi
-		
+
 		plt.subplot(3,1,1)
 		plt.plot(f, np.abs(H))
 		plt.title('magnitude')
-		
+
 		plt.subplot(3,1,2)
 		plt.plot(f, np.unwrap(np.angle(H)))
 		plt.title('phase')
-		
+
 		plt.subplot(3,1,3)
 		plt.plot(f, np.unwrap(np.angle(H))/(f*2*np.pi))
 		plt.title('delay')
-		
+
+		plt.figure()
+
+
+
 		plt.show()
